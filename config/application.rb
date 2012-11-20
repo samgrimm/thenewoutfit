86f9e9dd58ec6e392a06e01ca27dd66a3ab1b0fb
samantha <samantha.grimm@gmail.com>
2012-11-20 14:53:08 -0600

    trying to fix heroku errors

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Thenewoutfitproject
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    # to fix the heroku loading
+    config.assets.initialize_on_precompile = false
+    
   end
 end
